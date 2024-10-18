88145e4bb97a4c7aa63a6a1fb32501ad817d7f32
philipMartini <f.martini@campus.unimib.it>
2024-10-18 17:47:05 +0200

    Add microservice template

---
+++ microservice/delivery-microservice/src/main/java/com/reply/discovery/delivery_microservice/DeliveryMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.reply.discovery.delivery_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryMicroserviceApplication.class, args);
+	}
+
+}
---
+++ microservice/delivery-microservice/src/test/java/com/reply/discovery/delivery_microservice/DeliveryMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reply.discovery.delivery_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
