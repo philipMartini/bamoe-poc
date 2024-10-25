916692919742f415210427912b36fb646e555ab2
marberpp <m.bernardo@reply.it>
2024-10-25 17:46:13 +0200

    risolto il conflitto

--- microservice/delivery-microservice/src/main/java/com/reply/discovery/delivery_microservice/service/mapper/DeliveryMapper.java
+++ microservice/delivery-microservice/src/main/java/com/reply/discovery/delivery_microservice/service/mapper/DeliveryMapper.java
@@ -21,8 +21,27 @@ public class DeliveryMapper {
 	public Map<String,Object> createBamoeStartProcessRequestBody(StartDeliveryProcessRequest request) {
 		Map<String,Object> requestMap = new HashMap<>();
 		
+		/*/
+		Map<String,Object> processParameters = new HashMap<>();
+		boolean a = (processParameters.containsKey("APIWithAsyncCallback") && ((Boolean)(processParameters.get("APIWithAsyncCallback"))).equals(true));
+		boolean b = (processParameters.containsKey("APIWithAsyncCallback") && ((String)(processParameters.get("APIWithAsyncCallback"))).equals("true"));
+
+		if(processParameters.containsKey("APIWithAsyncCallback")){
+		    System.out.println("APIWithAsyncCallback:" + processParameters.get("APIWithAsyncCallback") + "--- classType" + processParameters.get("APIWithAsyncCallback").getClass());
+		}
+
+		
+		//*/
+		/*/
+		boolean a = (requestMap.containsKey("APIWithAsyncCallback") && ((boolean)(requestMap.get("APIWithAsyncCallback")))==true);
+		boolean b = ((String)(requestMap.get("APIWithAsyncCallback"))).equals("true");
+		boolean c = ((boolean)(requestMap.get("APIWithAsyncCallback")))==true;
+		boolean d = (requestMap.containsKey("APIWithAsyncCallback") && ((String)requestMap.get("APIWithAsyncCallback")==true));
+		//*/
+		
 		ProcessParameters processParameters = new ProcessParameters();
-		processParameters.setApIurl("http://192.168.178.102:8088/customers/" + request.getCustomerId());
+		processParameters.setApIUrl("http://192.168.178.102:8088/customers/" + request.getCustomerId());
+		processParameters.setApIWithAsyncCallback(false);
 		requestMap.put("processRequest", request);
 		requestMap.put("processParameters", processParameters);
 		//JSONObject jsonObject = new JSONObject(requestMap);
